9173647fd5d8936a7d8caa521ab5afd968cbf279
thomasfanto <thomas.fanto@consid.se>
2019-10-29 15:01:09 +0100

    x

--- src/main/java/se/havochvatten/uvms/simulator/TimerBean.java
+++ src/main/java/se/havochvatten/uvms/simulator/TimerBean.java
@@ -19,6 +19,7 @@ import java.util.List;
 // execute           :  java -jar UVMS-NeafcSimulator-1.0-SNAPSHOT-runner
 // stop              :  ctrl-c
 //
+// mvnw quarkus:generate-config -Dfile=TESTapplication.properties
 
 
 // list available extentions mvnw quarkus:list-extensions
